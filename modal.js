1ef411d60c8dc4f721d50f398a3ef2253357fff9
Nolan <nolanb414@yahoo.com>
2023-02-24 21:30:18 -0500

    User can add and save widgets

--- modal.js
+++ modal.js
@@ -5,6 +5,10 @@ let aboutClose = document.getElementById("about_close_btn");
 let resultModal = document.getElementById("result_modal");
 let resultClose = document.getElementById("result_close_btn");
 
+let widgetModal = document.getElementById("widget_modal");
+let widgetOpen = document.getElementById("add_widget");
+let widgetClose = document.getElementById("widget_close_btn")
+
 aboutOpen.onclick = function() {
     aboutModal.style.display = "block";
 }
@@ -17,10 +21,20 @@ resultClose.onclick = function() {
     resultModal.style.display = "none";
 }
 
+widgetOpen.onclick = function() {
+    widgetModal.style.display = "block";
+}
+
+widgetClose.onclick = function() {
+    widgetModal.style.display = "none";
+}
+
 window.onclick = function(event) {
     if (event.target == aboutModal) {
         aboutModal.style.display = "none";
     } else if (event.target == resultModal) {
         resultModal.style.display = "none";
+    } else if (event.target == widgetModal) {
+        widgetModal.style.display = "none";
     }
 }
