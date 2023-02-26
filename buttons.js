f80d2147280e54fb4833d1d073bec6e238bb88f2
Nolan <nolanb414@yahoo.com>
2023-02-26 10:31:49 -0500

    Now only real words will be foound on random word

--- buttons.js
+++ buttons.js
@@ -5,7 +5,7 @@ searchButton.onclick = document.getElementById("search_form").onsubmit;
 let feelingLucky = document.getElementById("random_word");
 
 async function getRandomWord() {
-    const url = "https://random-word-api.herokuapp.com/word?length=5";
+    const url = "https://random-word-api.herokuapp.com/word?length=5&number=10";
 
     let obj;
 
@@ -16,8 +16,26 @@ async function getRandomWord() {
     return obj[0];
 }
 
+async function isWord(word) {
+    const URL = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
+
+    const res = await fetch(URL, {method: "HEAD"});
+
+    return (res.status == 200);
+}
+
 feelingLucky.onclick = async function() {
-    let randomWord = await getRandomWord();
+    // Check to make sure that it is a word!
+
+    let randomWord;
+
+    while (true) {
+        randomWord = await getRandomWord(); // get another word
+        let isValidWord = await isWord(randomWord);
+        if (isValidWord) {
+            break;
+        }
+    }
 
 
     document.getElementById("search_field").value = randomWord;
